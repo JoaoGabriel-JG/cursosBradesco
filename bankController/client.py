e1f4240122aa206decae215dbc615db84cdf20a8
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-04-14 10:07:04 -0300

    att

--- bankController/client.py
+++ bankController/client.py
@@ -2,3 +2,9 @@ class Cliente:
     def __init__(self, n, fone):
         self.nome = n
         self.telefone = fone
+
+class Conta:
+    def __init__(self, titular, numero, saldo):
+        self.saldo = 0
+        self.numero = numero
+        self.titular = titular
--- bankController/main.py
+++ bankController/main.py
@@ -1,6 +1,7 @@
-from client import Cliente
+from client import Cliente, Conta
 
 c1 = Cliente('Leonardo', '119999-9999')
+conta = Conta(c1.nome , 6565, 0)
 
-print(c1)
 print(c1.nome, ' e ', c1.telefone)
+print(conta.titular, ' número: ', conta.numero, ' seu saldo: ', conta.saldo)
